f2c76742a4820f5ddef2097a51cbeb8bd9887250
Rico Tubbing <rico12978@hotmail.com>
2019-03-29 16:02:51 +0100

    Update make some noise model

--- models/VGG/MakeSomeNoise.py
+++ models/VGG/MakeSomeNoise.py
@@ -19,50 +19,55 @@ class MakeSomeNoise(nn.Module):
         self.max_pool = 2
         self.channel_size = channel_size
 
-        self.conv1_channels = self.channel_size         # 8
-        self.conv2_channels = self.conv1_channels * 2   # 16
-        self.conv3_channels = self.conv2_channels * 2   # 32
-        self.conv4_channels = self.conv3_channels * 2   # 64
-        self.conv5_channels = self.conv4_channels * 2   # 128
-        self.conv6_channels = self.conv5_channels       # 128
-        self.conv7_channels = self.conv5_channels       # 128
-        self.conv8_channels = self.conv5_channels * 2   # 256
-        self.conv9_channels = self.conv5_channels       # 256
-        self.conv10_channels = self.conv5_channels      # 256
+        self.conv1_channels = self.channel_size  # 8
+        self.conv2_channels = self.conv1_channels * 2  # 16
+        self.conv3_channels = self.conv2_channels * 2  # 32
+        self.conv4_channels = self.conv3_channels * 2  # 64
+        self.conv5_channels = self.conv4_channels * 2  # 128
+        self.conv6_channels = self.conv5_channels  # 128
+        self.conv7_channels = self.conv6_channels  # 128
+        self.conv8_channels = self.conv7_channels * 2  # 256
+        self.conv9_channels = self.conv8_channels  # 256
+        self.conv10_channels = self.conv9_channels  # 256
         num_features = input_shape
 
         # First BN
         self.bn0 = nn.BatchNorm1d(num_features=1).to(device)
 
         # Convolutions + BN + MP
-        self.conv1_1 = nn.Conv1d(1, self.conv1_channels,
-                                 kernel_size=self.kernel_size, padding=self.padding).to(device)
-        self.conv1_2 = nn.Conv1d(self.conv1_channels, self.conv2_channels,
-                                 kernel_size=self.kernel_size, padding=self.padding).to(device)
-        self.bn1 = nn.BatchNorm1d(num_features=self.conv2_channels).to(device)
+        self.conv1 = nn.Conv1d(1, self.conv1_channels,
+                               kernel_size=self.kernel_size, padding=self.padding).to(device)
+        self.conv2 = nn.Conv1d(self.conv1_channels, self.conv2_channels,
+                               kernel_size=self.kernel_size, padding=self.padding).to(device)
+        self.conv3 = nn.Conv1d(self.conv2_channels, self.conv3_channels,
+                               kernel_size=self.kernel_size, padding=self.padding).to(device)
+        self.conv4 = nn.Conv1d(self.conv3_channels, self.conv4_channels,
+                               kernel_size=self.kernel_size, padding=self.padding).to(device)
+        self.conv5 = nn.Conv1d(self.conv4_channels, self.conv5_channels,
+                               kernel_size=self.kernel_size, padding=self.padding).to(device)
+        self.conv6 = nn.Conv1d(self.conv5_channels, self.conv6_channels,
+                               kernel_size=self.kernel_size, padding=self.padding).to(device)
+        self.conv7 = nn.Conv1d(self.conv6_channels, self.conv7_channels,
+                               kernel_size=self.kernel_size, padding=self.padding).to(device)
+        self.conv8 = nn.Conv1d(self.conv7_channels, self.conv8_channels,
+                               kernel_size=self.kernel_size, padding=self.padding).to(device)
+        self.conv9 = nn.Conv1d(self.conv8_channels, self.conv9_channels,
+                               kernel_size=self.kernel_size, padding=self.padding).to(device)
+        self.conv10 = nn.Conv1d(self.conv9_channels, self.conv10_channels,
+                                kernel_size=self.kernel_size, padding=self.padding).to(device)
+
+        self.bn1 = nn.BatchNorm1d(num_features=self.conv1_channels).to(device)
+        self.bn3 = nn.BatchNorm1d(num_features=self.conv3_channels).to(device)
+        self.bn5 = nn.BatchNorm1d(num_features=self.conv5_channels).to(device)
+        self.bn7 = nn.BatchNorm1d(num_features=self.conv7_channels).to(device)
+        self.bn9 = nn.BatchNorm1d(num_features=self.conv9_channels).to(device)
         self.mp1 = nn.MaxPool1d(self.max_pool).to(device)
         num_features = int(num_features / 2)
-
-        # CNN + BN
-        self.conv2_1 = nn.Conv1d(self.conv2_channels, self.conv3_channels,
-                                 kernel_size=3, padding=1).to(device)
-        self.conv2_2 = nn.Conv1d(self.conv3_channels, self.conv4_channels,
-                                 kernel_size=3, padding=1).to(device)
-        num_features = int(num_features)
-        self.bn2 = nn.BatchNorm1d(num_features=self.conv4_channels).to(device)
-
-        # CNN + BN
-        self.conv3_1 = nn.Conv1d(self.conv4_channels, self.conv5_channels,
-                                 kernel_size=3, padding=1).to(device)
-        self.conv3_2 = nn.Conv1d(self.conv5_channels, self.conv6_channels,
-                                 kernel_size=3, padding=1).to(device)
-        num_features = int(num_features)
-        self.bn3 = nn.BatchNorm1d(num_features=self.conv6_channels).to(device)
 
         # Dropout
         self.drop_out = nn.Dropout(p=0.5)
 
-        self.fc4 = torch.nn.Linear(int(self.conv6_channels * num_features), 256).to(device)
+        self.fc4 = torch.nn.Linear(int(self.conv10_channels * num_features), 256).to(device)
         self.fc5 = torch.nn.Linear(256, 256).to(device)
         self.fc6 = torch.nn.Linear(256, self.out_shape).to(device)
 
@@ -72,11 +77,12 @@ class MakeSomeNoise(nn.Module):
 
         x = self.bn0(inputs)
         # x = inputs
-        x = self.mp1(self.bn1(self.conv1_2(self.conv1_1(x))))
-        x = self.bn2(self.conv2_2(self.conv2_1(x)))
-        x = self.bn3(
-            self.conv3_2(
-                self.conv3_1(x)))
+        x = self.bn1(self.mp1(F.relu(self.conv1(x))))
+        x = self.bn3(F.relu(self.conv3(F.relu(self.conv2(x)))))
+        x = self.bn5(F.relu(self.conv5(F.relu(self.conv4(x)))))
+        x = self.bn7(F.relu(self.conv7(F.relu(self.conv6(x)))))
+        x = self.bn9(F.relu(self.conv9(F.relu(self.conv8(x)))))
+        x = F.relu(self.conv10(x))
 
         # Reshape data for classification
         x = x.view(batch_size, -1)
@@ -85,12 +91,8 @@ class MakeSomeNoise(nn.Module):
         x = self.drop_out(x)
 
         # Perform MLP
-        x = self.fc4(x).to(device)
-        x = F.relu(x).to(device)
-        x = self.drop_out(x)
-        x = self.fc5(x).to(device)
-        x = self.drop_out(x)
-        x = F.relu(x).to(device)
+        x = F.relu(self.fc4(x)).to(device)
+        x = F.relu(self.fc5(x)).to(device)
 
         # Final layer without ReLU
         x = self.fc6(x).to(device)
